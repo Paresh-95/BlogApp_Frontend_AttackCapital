cb95f7a64bd53fc6492fa886236925bd3ef7fedd
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-23 21:41:17 +0530

    changs in next middlewaree

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,33 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
+  const token = request.cookies.get('token') 
 
-  const isAuthPath = path === '/login' || path === '/sign-up'
-  const isHomePath = path === '/'
+  console.log('Request Path:', path)
+  console.log('Token:', token) 
 
-  const token = request.cookies.get('token')?.value || ''
+  const isAuthPath = path === '/login' || path === '/sign-up'  
+  const isHomePath = path === '/'  
+  const isDashboardPath = path === '/dashboard'  
+
 
   if (!isAuthPath && !isHomePath && !token) {
+    console.log('Redirecting to login because no token is found')
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
+  
   if (isAuthPath && token) {
+    console.log('Redirecting to dashboard because token exists')
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
+  
   return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',  
   ],
 }
-
