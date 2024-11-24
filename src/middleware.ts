48d34af2699cbb290d87f5cec6e3f8ca709b828b
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-24 19:07:37 +0530

    solved cookie issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
+import * as cookie from 'cookie';  
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token'); 
+  const cookies = cookie.parse(req.headers.get('cookie') || '');
+  const token = cookies.token; 
+
   const { pathname } = req.nextUrl;
 
   console.log('Pathname:', pathname); 
@@ -10,21 +13,18 @@ export function middleware(req: NextRequest) {
   const loginUrl = new URL('/login', req.url);
   const dashboardUrl = new URL('/dashboard', req.url);
 
-  
   if (pathname.startsWith('/dashboard') && !token) {
     console.log('No token, redirecting to /login');
     loginUrl.searchParams.set('reason', 'unauthorized'); 
     return NextResponse.redirect(loginUrl);
   }
 
-  
   if ((pathname.startsWith('/login') || pathname.startsWith('/sign-up')) && token) {
     console.log('Token exists, redirecting to /dashboard');
     dashboardUrl.searchParams.set('reason', 'already_authenticated'); 
     return NextResponse.redirect(dashboardUrl);
   }
 
-  
   return NextResponse.next();
 }
 
